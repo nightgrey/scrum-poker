8442d10f54eb4cb960321221cf4cc4fafd9d304f
Nico <nico.niebergall@googlemail.com>
2020-02-24 21:21:13 +0100

    Removed: Forgotten `react-redux-firebase` import. Whoops.

--- store.js
+++ store.js
@@ -1,9 +1,7 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { firebaseReducer } from 'react-redux-firebase';
 
 const reducers = combineReducers({
-  firebase: firebaseReducer,
 });
 
 export default (preloadedState = {}) => createStore(
