29ee63530314e75e6af7603c2f59a1f42bff272a
Nico <nico.niebergall@googlemail.com>
2020-02-25 12:14:54 +0100

    Added: `next-optimized-images` and `svgr`

--- next.config.js
+++ next.config.js
@@ -1,48 +1,53 @@
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
 const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require('next/constants');
 const merge = require('lodash/merge');
+const withSvgr = require('./lib/withSvgr');
 
-module.exports = (phase) => {
-  const configuration = {
-    env: {
-      title: 'Scrum Poker',
-    },
-  };
-
-  const developmentConfiguration = {
-    env: {
-      firebaseConfig: {
-        apiKey: 'AIzaSyBwOiQL7wZ59CWzM_KPG6YVOeI2igMowQY',
-        authDomain: 'scrum-poker-d0059.firebaseapp.com',
-        databaseURL: 'https://scrum-poker-d0059.firebaseio.com',
-        projectId: 'scrum-poker-d0059',
-        storageBucket: 'scrum-poker-d0059.appspot.com',
-        messagingSenderId: '413486540736',
-        appId: '1:413486540736:web:3481a2775a001741b70a25',
-      },
-    },
-  };
-
-  const productionConfiguration = {
-    env: {
-      firebaseConfig: {
-        apiKey: 'AIzaSyBwOiQL7wZ59CWzM_KPG6YVOeI2igMowQY',
-        authDomain: 'scrum-poker-d0059.firebaseapp.com',
-        databaseURL: 'https://scrum-poker-d0059.firebaseio.com',
-        projectId: 'scrum-poker-d0059',
-        storageBucket: 'scrum-poker-d0059.appspot.com',
-        messagingSenderId: '413486540736',
-        appId: '1:413486540736:web:7321600fb86fda25b70a25',
-      },
-    },
-  };
-
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return merge(configuration, developmentConfiguration);
-  }
-
-  if (phase === PHASE_PRODUCTION_BUILD) {
-    return merge(configuration, productionConfiguration);
-  }
-
-  return configuration;
+const allConfiguration = {
+  env: {
+    title: 'Scrum Poker',
+  },
 };
+
+const developmentConfiguration = merge(allConfiguration, {
+  env: {
+    firebaseConfig: {
+      apiKey: 'AIzaSyBwOiQL7wZ59CWzM_KPG6YVOeI2igMowQY',
+      authDomain: 'scrum-poker-d0059.firebaseapp.com',
+      databaseURL: 'https://scrum-poker-d0059.firebaseio.com',
+      projectId: 'scrum-poker-d0059',
+      storageBucket: 'scrum-poker-d0059.appspot.com',
+      messagingSenderId: '413486540736',
+      appId: '1:413486540736:web:3481a2775a001741b70a25',
+    },
+  },
+});
+
+const productionConfiguration = merge(allConfiguration, {
+  env: {
+    firebaseConfig: {
+      apiKey: 'AIzaSyBwOiQL7wZ59CWzM_KPG6YVOeI2igMowQY',
+      authDomain: 'scrum-poker-d0059.firebaseapp.com',
+      databaseURL: 'https://scrum-poker-d0059.firebaseio.com',
+      projectId: 'scrum-poker-d0059',
+      storageBucket: 'scrum-poker-d0059.appspot.com',
+      messagingSenderId: '413486540736',
+      appId: '1:413486540736:web:7321600fb86fda25b70a25',
+    },
+  },
+});
+
+module.exports = withPlugins(
+  [
+    [optimizedImages, {
+      // Let `svgr` handle <svg>'s.
+      handleImages: ['jpeg', 'png', 'webp', 'gif'],
+    }],
+    [withSvgr, {}],
+  ],
+  {
+    [PHASE_PRODUCTION_BUILD]: productionConfiguration,
+    [PHASE_DEVELOPMENT_SERVER]: developmentConfiguration,
+  },
+);
