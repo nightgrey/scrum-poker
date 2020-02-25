5a4cbdf520ae15bbb76b74b109a008576d94c42c
Nico <nico.niebergall@googlemail.com>
2020-02-25 21:23:57 +0100

    Fixed: App prop-types

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import '../styles/index.css';
 const App = ({ Component, pageProps }) => <Component {...pageProps} />;
 
 App.propTypes = {
-  Component: PropTypes.element,
+  Component: PropTypes.oneOfType([PropTypes.element, PropTypes.func]),
   pageProps: PropTypes.object,
 };
 
