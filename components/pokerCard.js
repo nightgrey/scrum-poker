d5bd0db9c45ad60f8719c668b7c729f682666322
Nico <nico.niebergall@googlemail.com>
2020-02-25 16:12:27 +0100

    Changed: Poker card layout

--- components/pokerCard.js
+++ components/pokerCard.js
@@ -3,33 +3,43 @@ import PropTypes from 'prop-types';
 import HeartIcon from '../images/icon-heart.svg';
 import IntrinsicRatio from './intrinsicRatio';
 
-const PokerCard = ({ value, className, ...props }) => (
-  <IntrinsicRatio ratio={16 / 9} className={className} {...props}>
-    <div className="h-40 bg-gray-800 text-white rounded-lg relative flex flex-col h-full justify-between">
-      <div className="flex justify-start p-2">
-        <div className="w-1/6 text-center">
-          <span className="text-sm">{value}</span><br />
-          <HeartIcon className="inline-block stroke-current text-primary w-full" />
+const PokerCard = ({
+  value, size, className, ...props
+}) => (
+  <div className={className} style={{ fontSize: `${size}em` }}>
+    <div style={{ maxWidth: '12em' }}>
+      <IntrinsicRatio ratio={16 / 9} {...props}>
+        <div className="h-40 bg-gray-800 text-white rounded-lg relative flex flex-col h-full justify-between">
+          <div className="flex justify-start" style={{ padding: '.75em 1em' }}>
+            <div className="text-center">
+              <span className="leading-none" style={{ fontSize: '1em' }}>{value}</span>
+              <br />
+              <HeartIcon style={{ width: '2em' }} className="inline-block stroke-current text-primary w-full" />
+            </div>
+          </div>
+          <span style={{ fontSize: '3.5em' }} className="text-center">{value}</span>
+          <div className="flex justify-end" style={{ padding: '.75em 1em' }}>
+            <div className="text-center" style={{ transform: 'rotate(180deg)' }}>
+              <span className="leading-none" style={{ fontSize: '1em' }}>{value}</span>
+              <br />
+              <HeartIcon style={{ width: '2em' }} className="inline-block stroke-current text-primary w-full" />
+            </div>
+          </div>
         </div>
-      </div>
-      <span className="text-6xl text-center">{value}</span>
-      <div className="flex justify-end p-2">
-        <div className="w-1/6 text-center" style={{ transform: 'rotate(180deg)' }}>
-          <span className="text-sm">{value}</span><br />
-          <HeartIcon className="inline-block stroke-current text-primary w-full" />
-        </div>
-      </div>
+      </IntrinsicRatio>
     </div>
-  </IntrinsicRatio>
+  </div>
 );
 
 PokerCard.propTypes = {
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  size: PropTypes.number,
   className: PropTypes.string,
 };
 
 PokerCard.defaultProps = {
   className: null,
+  size: 1,
 };
 
 export default PokerCard;
