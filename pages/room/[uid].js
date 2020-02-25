a29e7bc36ff697ba74e9135899b052017b794d78
Nico <nico.niebergall@googlemail.com>
2020-02-25 21:31:15 +0100

    Added: Get rooms via query parameter from firebase

--- pages/room/[uid].js
+++ pages/room/[uid].js
@@ -1,18 +1,57 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { useRouter } from 'next/router';
 import Layout from '../../components/layout';
 import withRedux from '../../lib/withRedux';
+import firebase from '../../lib/firebase';
 
-const Room = () => {
+
+const Room = ({ rooms }) => {
   const router = useRouter();
   const { uid } = router.query;
 
   return (
     <Layout>
-      Room:
-      {uid}
+      <div className="container mx-auto">
+        {!rooms ? (
+          <span>
+            No room with uid {uid} found.
+          </span>
+        ) : Object.keys(rooms).map((roomsKey) => (
+          <div key={roomsKey}>
+            {Object.keys(rooms[roomsKey]).map((roomsPropertyKey) => (
+              <span>
+                {roomsPropertyKey}: {rooms[roomsKey][roomsPropertyKey].toString()}
+                <br />
+              </span>
+            ))}
+          </div>
+        ))}
+      </div>
     </Layout>
   );
+};
+
+Room.propTypes = {
+  rooms: PropTypes.objectOf(PropTypes.shape({
+    uid: PropTypes.string,
+    title: PropTypes.string,
+  })),
+};
+
+Room.defaultProps = {
+  rooms: null,
+};
+
+Room.getInitialProps = async function ({ query }) {
+  const { uid } = query;
+
+  const room = firebase.database().ref('/rooms');
+  const snapshot = await room.orderByChild('uid').equalTo(uid).once('value');
+
+  return {
+    rooms: snapshot.val(),
+  };
 };
 
 export default withRedux(Room);
